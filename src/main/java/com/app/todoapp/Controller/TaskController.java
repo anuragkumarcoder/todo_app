1bbd228ae3671b74412e1555c7eaa04c4fb2c1dd
anurag <anuragkumar78702685001@gmail.com>
2026-04-08 23:14:58 +0530

    done everything css is done from bootstrap

--- src/main/java/com/app/todoapp/Controller/TaskController.java
+++ src/main/java/com/app/todoapp/Controller/TaskController.java
@@ -2,10 +2,13 @@ package com.app.todoapp.Controller;
 
 import com.app.todoapp.Models.Tasks;
 import com.app.todoapp.Service.ToDoService;
-import com.app.todoapp.Models.Task; // Use your actual Task class path here
+import com.app.todoapp.Models.Tasks; // Use your actual Task class path here
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model; // Correct Model import
 import org.springframework.web.bind.annotation.GetMapping; // Required for mapping
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,4 +27,22 @@ public class TaskController {
         model.addAttribute("tasks", tasks);
         return "tasks";
     }
+    @PostMapping("/tasks")
+    public String CreateTask(@RequestParam String title) {
+      toDoService.createTask(title);
+        return "redirect:/tasks";
+    }
+    @GetMapping("/{id}/delete")
+    public String deleteTasks(@PathVariable Long id) {
+        toDoService.deleteTask(id);
+
+        return "redirect:/tasks";
+    }
+        @GetMapping("/{id}/toggle")
+        public String toggleTasks(@PathVariable Long id) {
+            toDoService.toggleTask(id);
+
+            return "redirect:/tasks";
+    }
+
 }
--- src/main/java/com/app/todoapp/Service/ToDoService.java
+++ src/main/java/com/app/todoapp/Service/ToDoService.java
@@ -3,9 +3,10 @@ package com.app.todoapp.Service;
 import com.app.todoapp.Models.Tasks;
 import com.app.todoapp.Repository.TaskRepo;
 import org.springframework.scheduling.config.Task;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ToDoService {
     private final TaskRepo taskRepo;
 
@@ -13,7 +14,26 @@ public class ToDoService {
         this.taskRepo = taskRepo;
     }
 
+    public  void toggleTask(Long id) {
+        Tasks task=taskRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("invalid task id"));
+                task.setComplete(!task.isComplete());
+        taskRepo.save(task);
+
+    }
+
+    public  void deleteTask(Long id) {
+        taskRepo.deleteById(id);
+    }
+
     public List<Tasks> getAllTasks() {
         return taskRepo.findAll();
+    }
+
+    public void createTask(String title) {
+        Tasks task=new Tasks();
+        task.setTitle(title);
+        task.setComplete(false);
+        taskRepo.save(task);
     }
 }
